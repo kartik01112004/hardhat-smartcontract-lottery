d557f645debe8abf5cfa4abd9bf4f660c7baec6a
kartik01112004 <kartikgoel018@gmail.com>
2024-03-08 16:02:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#Hardhat Lottery
+
+`"printWidth": 100`
+just changes how log a line can be beofre it changes to another line
+
+insted of storing strings we store error codes as its more gas efficent
---
+++ contracts/Raffel.sol
@@ -0,0 +1,38 @@
+// Raffle
+//enter lottery (pay some ammount)
+//pic a randome winner (varifiably random)
+//winner to be selected every x amount of time automaticaly
+//chainlink oracle -> for randomeness, automated execution(chainlink keepers)
+
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+error Raffle__notEnoughETHEnterd();
+
+contract Raffle {
+    //state variables
+    uint256 private immutable i_enteranceFee;
+    address payable[] private s_players;
+
+    constructor(uint256 enteranceFee) {
+        i_enteranceFee = enteranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_enteranceFee) {
+            revert Raffle__notEnoughETHEnterd(); //just for knowing that they enterd enough eth to participate
+        }
+        s_players.push(payable(msg.sender));
+    }
+
+    // function picRandomeWinner() {}
+
+    function getEnternceFee() public view returns (uint256) {
+        return i_enteranceFee;
+    }
+
+    function getPlayers(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
