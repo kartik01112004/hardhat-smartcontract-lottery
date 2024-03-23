c2f316f8c42e75e08a67e9db0c57171b82714cdb
kartik01112004 <kartikgoel018@gmail.com>
2024-03-23 23:29:21 +0530

    updates

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,12 +1,24 @@
-const { network } = require("hardhat");
+const { network, ethers } = require("hardhat");
+const { developmentChains, networkConfig } = require("../helper-hardhat-config");
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployements;
     const { deployer } = await getNamedAccounts();
+    const chainId = network.config.chainId;
+    let vrfCoordinatorV2Address;
 
+    if (developmentChains.include(network.name)) {
+        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
+        vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address;
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"];
+    }
+
+    const enteranceFee = networkConfig[chainId]["enteranceFee"];
+    const args = [];
     const raffle = await deploy("Raffle", {
         from: deployer,
-        args: [],
+        args: args,
         log: true,
         waitConfermations: network.config.blockConfermations || 1,
     });
--- hardhat.config.js
+++ hardhat.config.js
@@ -37,21 +37,21 @@ module.exports = {
             saveDeployments: true,
             chainId: 11155111,
         },
-        mainnet: {
-            url: MAINNET_RPC_URL,
-            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            //   accounts: {
-            //     mnemonic: MNEMONIC,
-            //   },
-            saveDeployments: true,
-            chainId: 1,
-        },
-        polygon: {
-            url: POLYGON_MAINNET_RPC_URL,
-            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            saveDeployments: true,
-            chainId: 137,
-        },
+        // mainnet: {
+        //     url: MAINNET_RPC_URL,
+        //     accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+        //     //   accounts: {
+        //     //     mnemonic: MNEMONIC,
+        //     //   },
+        //     saveDeployments: true,
+        //     chainId: 1,
+        // },
+        // polygon: {
+        //     url: POLYGON_MAINNET_RPC_URL,
+        //     accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+        //     saveDeployments: true,
+        //     chainId: 137,
+        // },
     },
     solidity: "0.8.7",
     namedAccounts: {
