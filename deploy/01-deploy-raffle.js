2d07a534c54ad26c305fdd2e113039e5c2e34b8e
kartik01112004 <kartikgoel018@gmail.com>
2024-04-12 23:23:29 +0530

    updates

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -5,17 +5,23 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployements;
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId;
-    let vrfCoordinatorV2Address;
+    let vrfCoordinatorV2Address, subscripionId;
 
     if (developmentChains.include(network.name)) {
         const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
         vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address;
+        const transactionResponse = await VRFCoordinatorV2Mock.createSubscripion();
+        const transactionReceipt = await transactionResponse.wait(1);
+        subscripionId = transactionReceipt.events[0].args.subId;
+        //fund the subscription
+        await VRFCoordinatorV2Mocvk.fundSubscription(subscripionId);
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"];
     }
 
     const enteranceFee = networkConfig[chainId]["enteranceFee"];
-    const args = [];
+    const gasLane = networkConfig[chainId]["gasLane"];
+    const args = [vrfCoordinatorV2Address, enteranceFee, gasLane];
     const raffle = await deploy("Raffle", {
         from: deployer,
         args: args,
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -5,10 +5,12 @@ const networkConfig = {
         name: "sepolia",
         vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
         enteranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
     },
     31337: {
         name: "hardhat",
         enteranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
     },
 };
 
