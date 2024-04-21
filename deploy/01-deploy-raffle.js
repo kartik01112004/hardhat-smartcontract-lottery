21ec7136e9c4a4f2390ea88b03df6741bdb47817
kartik01112004 <kartikgoel018@gmail.com>
2024-04-21 23:05:52 +0530

    deployement done!

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,31 +1,53 @@
 const { network, ethers } = require("hardhat");
 const { developmentChains, networkConfig } = require("../helper-hardhat-config");
+const { verify } = require("../helper-hardhat-config");
+
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2");
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
-    const { deploy, log } = deployements;
+    const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId;
     let vrfCoordinatorV2Address, subscripionId;
 
-    if (developmentChains.include(network.name)) {
+    if (developmentChains.includes(network.name)) {
         const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
         vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address;
-        const transactionResponse = await VRFCoordinatorV2Mock.createSubscripion();
+        const transactionResponse = await VRFCoordinatorV2Mock.createSubscription();
         const transactionReceipt = await transactionResponse.wait(1);
         subscripionId = transactionReceipt.events[0].args.subId;
         //fund the subscription
-        await VRFCoordinatorV2Mocvk.fundSubscription(subscripionId);
+        await VRFCoordinatorV2Mock.fundSubscription(subscripionId, VRF_SUB_FUND_AMOUNT);
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"];
+        subscripionId = networkConfig[chainId]["subscripionId"];
     }
 
     const enteranceFee = networkConfig[chainId]["enteranceFee"];
     const gasLane = networkConfig[chainId]["gasLane"];
-    const args = [vrfCoordinatorV2Address, enteranceFee, gasLane];
+    const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"];
+    const interval = networkConfig[chainId]["interval"];
+
+    const args = [
+        vrfCoordinatorV2Address,
+        enteranceFee,
+        gasLane,
+        subscripionId,
+        callbackGasLimit,
+        interval,
+    ];
     const raffle = await deploy("Raffle", {
         from: deployer,
         args: args,
         log: true,
         waitConfermations: network.config.blockConfermations || 1,
     });
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying....");
+        await verify(raffle.address, args);
+    }
+    log("------------------------");
 };
+
+module.exports.tags = ["all", "raffle"];
